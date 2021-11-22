caea82984a7a66b395230713e669589da05c984a
mohammad-miftakhus-sholikin <mohammadmiftakhussholikin@gmail.com>
2021-11-22 09:19:05 +0700

    Signed-off-by: mohammad-miftakhus-sholikin <mohammadmiftakhussholikin@gmail.com>

--- Microsoft.PowerShell_profile.ps1
+++
@@ -1,9 +0,0 @@
-# Anaconda profile
-#region conda initialize
-# !! Contents within this block are managed by 'conda init' !!
-(& "C:\Users\moham\anaconda3\Scripts\conda.exe" "shell.powershell" "hook") | Out-String | Invoke-Expression
-#endregion
-
-# Greeting command
-Write-Host "Halo, Sholikin!" -ForegroundColor red -BackgroundColor white
-# Write-Host "Sekarang" -ForegroundColor red -BackgroundColor white; Get-Date
